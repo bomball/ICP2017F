b533ef282f6c7e2475220a0db00eed99ce10e0ee
bomball <31543179+bomball@users.noreply.github.com>
2017-12-13 13:22:24 -0600

    Update main.m

--- Final Project/src/main.m
+++ Final Project/src/main.m
@@ -8,4 +8,6 @@ figure;
 ObtainingErrorWithGompertzianFit();
 hold on;
 RegressionModel();
+saveas(gcf,'Gompertzian_Fit_with_Regression_Model.png'); %because its a
+%combination of two scripts, it can only be saved in the main script
 end
